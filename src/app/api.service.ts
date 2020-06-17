4340a72c1707e3741069f26edc33c484cd7ba0e5
Chris Lane <pankokrumbs@gmail.com>
2020-06-17 11:30:16 -0600

    Refactor to getCharacters()

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,11 +7,14 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
 
   API_Key = 'cdaa672b867e4bb0441e68efcb19a8c1';
+  ts = 'thesoer';
+  hash = '4a161dc76ced1c6bebdebdbf3041c028'
   constructor(private httpClient: HttpClient) { 
     
   }
 
-  public getData() {
-    return this.httpClient.get(`https://gateway.marvel.com/`)
+  public getCharacters() {
+    return this.httpClient.get(`https://gateway.marvel.com/v1/public/characters?ts=${this.ts}&apikey=${this.API_Key}&hash=${this.hash}
+`)
   }
 }
